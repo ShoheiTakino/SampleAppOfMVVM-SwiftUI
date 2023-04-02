ed78927f6587d089c3c44ff2135968058e2bac85
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-02 12:24:27 +0900

    Initial Commit

---
+++ SampleAppOfMVVM+SwiftUI/SampleAppOfMVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppOfMVVM_SwiftUIApp.swift
+//  SampleAppOfMVVM+SwiftUI
+//
+//  Created by 滝野翔平 on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppOfMVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
